cbc6c92bf76054c61465c79334510373ce797f6f
vvaldesc <daw_victorvaldes@riberadeltajo.es>
2024-10-12 01:16:56 +0200

    Recursive mine counter

--- buscaminas.py
+++ buscaminas.py
@@ -1,9 +1,22 @@
 import numpy as np
 
-table=np.random.randint(-1, 1, size=(8, 8))
+searching_vectors = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
+table = np.random.randint(-1, 1, size=(8, 8))
+
+def how_many_mines(table,i,j,searching_vectors,c=0):
+    if not searching_vectors:
+        return c
+    else:
+        ni, nj = i + searching_vectors[0][0], j + searching_vectors[0][1]
+        if 0 <= ni < table.shape[0] and 0 <= nj < table.shape[1]:
+            if table[ni,nj] == -1:
+                c += 1
+        return how_many_mines(table,i,j,searching_vectors[1:],c)
+
 
 def count_cell_mines(table,i=0,j=0):
-    print(table[i][j])
+    if table[i][j] != -1:
+        table[i][j] = how_many_mines(table,i,j,searching_vectors)
     if i < table.shape[0] - 1:
         i = i + 1
         return count_cell_mines(table,i,j)
@@ -16,5 +29,5 @@ def count_cell_mines(table,i=0,j=0):
             return table
 
 
-
+print(table)
 print(count_cell_mines(table))
