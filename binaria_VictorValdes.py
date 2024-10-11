1fcbe8d2344d5abb01d4c8871116bcd18e0356db
vvaldesc <daw_victorvaldes@riberadeltajo.es>
2024-10-11 11:55:28 +0200

    Binary search working

---
+++ binaria_VictorValdes.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+a=np.random.randint(0,2, size=500)
+b=np.arange(100)
+
+def binary_array_search(array,n:int,i=0):
+    if array.size > 0:
+        if array[0] == n:
+            return i
+        else:
+            array = np.delete(array,0)
+            return binary_array_search(array,n,i+1)
+    else:
+        return 'No se encontró la instancia'
+
+
+print(binary_array_search(a,1))
+print(binary_array_search(b,45))
+print(binary_array_search(b,200))
