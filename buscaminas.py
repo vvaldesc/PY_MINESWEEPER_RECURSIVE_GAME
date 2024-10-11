bd78540013c14dcca5c707fc7be35b867eb01d3d
vvaldesc <daw_victorvaldes@riberadeltajo.es>
2024-10-11 19:52:05 +0200

    Binary search matrix working

---
+++ buscaminas.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+table=np.random.randint(-1, 1, size=(8, 8))
+
+def count_cell_mines(table,i=0,j=0,checked=False):
+    print(table[i][j])
+    if i < table.shape[0] - 1:
+        i = i + 1
+        return count_cell_mines(table,i,j)
+    else:
+        if j < table.shape[1] - 1:
+            i = 0
+            j = j + 1
+            return count_cell_mines(table,i,j)
+        else:
+            return table
+
+
+
+print(count_cell_mines(table))
