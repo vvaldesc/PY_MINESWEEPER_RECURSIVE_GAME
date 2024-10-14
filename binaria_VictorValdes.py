25ee81fb6dbc4affcff2218982c753c45daf3ab9
vvaldesc <daw_victorvaldes@riberadeltajo.es>
2024-10-14 19:31:05 +0200

    Binary search working

--- binaria_VictorValdes.py
+++ binaria_VictorValdes.py
@@ -1,3 +1,7 @@
+'''
+
+If input array not sorted
+
 def binary_array_search(array,n:int,i=0):
     if array.size > 0:
         if array[0] == n:
@@ -6,3 +10,25 @@ def binary_array_search(array,n:int,i=0):
             return binary_array_search(array[1:],n,i+1)
     else:
         return False
+'''
+import numpy as np
+
+
+def binary_array_search(array,n:int,i=0):
+    if array.size > 0:
+        mid = array.size // 2
+        if array[mid] == n:
+            return i + mid
+        else:
+            if array[mid] > n:
+                return binary_array_search(array[:mid],n,i)
+            else:
+                #derecha
+                return binary_array_search(array[mid+1:],n,i + mid+1)
+    else:
+        return False
+
+
+array = np.arange(8)
+print(array)
+print(binary_array_search(array,7))
