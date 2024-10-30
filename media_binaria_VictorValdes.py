cd44b3251b373b31520b8e5a499a053992641bdb
vvaldesc <daw_victorvaldes@riberadeltajo.es>
2024-10-30 23:00:21 +0100

    Examples

--- media_binaria_VictorValdes.py
+++ media_binaria_VictorValdes.py
@@ -2,11 +2,11 @@ import numpy as np
 
 array = np.arange(11)
 
-def binary_array_average(array,s=0,i=0):
-    if array.size > 1:
-        return binary_array_average(array[:1],s+array[i-1],i+1)
+def binary_array_average(array: list):
+    if len(array)>0:
+        return array[0] + binary_array_average(array[1:])
     else:
-        return (s + array[i-1])/(i + 1)
+        return 0
 
 
 print(array)
